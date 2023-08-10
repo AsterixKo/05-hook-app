46e9f8617831448e9d46e3cdc5169a9487e85baf
Javier Moneo <jvr.moneo@gmail.com>
2023-08-10 17:37:38 +0200

    UseRef - Primer uso

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import { HooksApp } from "./HooksApp";
 import "./index.css";
+import { FocusScreen } from "./04-useRef/FocusScreen";
 // import { CounterApp } from "./01-useState/CounterApp";
 // import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
 // import { SimpleForm } from "./02-useEffect/SimpleForm";
 // import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
-import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
+// import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <MultipleCustomHooks/>
+  <FocusScreen />
   // </React.StrictMode>
 );
