7de4c8107649a8e5c903cb9c9f160362ccdcff5a
Javier Moneo <jvr.moneo@gmail.com>
2023-08-11 10:50:58 +0200

    Tarea memorize

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import { HooksApp } from "./HooksApp";
 import "./index.css";
-import { CallbackHook } from "./06-memos/CallbackHook";
+// import { CallbackHook } from "./06-memos/CallbackHook";
+import { Padre } from "./07-tarea-memo/Padre";
 // import { MemoHook } from "./06-memos/MemorizeHook";
 // import { Memorize } from "./06-memos/Memorize";
 // import { Layout } from "./05-useLayoutEffect/Layout";
@@ -15,6 +16,6 @@ import { CallbackHook } from "./06-memos/CallbackHook";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <CallbackHook />
+  <Padre />
   // </React.StrictMode>
 );
