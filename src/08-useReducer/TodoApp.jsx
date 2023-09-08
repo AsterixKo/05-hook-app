99e447a7c4ea1e6b891362e5a44afb2c4b0a0641
Javier Moneo <jvr.moneo@gmail.com>
2023-09-08 12:57:41 +0200

    Resolucion de la tarea - TodoApp

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,5 +1,7 @@
 import { useReducer } from "react";
 import { todoReducer } from "./todoReducer";
+import { TodoList } from "./TodoList";
+import { TodoAdd } from "./TodoAdd";
 
 const initialState = [
   {
@@ -29,36 +31,12 @@ export const TodoApp = () => {
 
       <div className="row">
         <div className="col-7">
-          {/* TodoList */}
-          <ul className="list-group">
-            {todos.map((todo) => (
-              // TodoItem ...
-              <li
-                key={todo.id}
-                className="list-group-item d-flex justify-content-between"
-              >
-                <span>Item 1</span>
-                <button className="btn btn-danger">Borrar</button>
-              </li>
-            ))}
-          </ul>
+          <TodoList todos={todos} />
         </div>
         <div className="col-5">
-          {/* TodoAdd onNewTodo(todo)  */}
-          {/* id: new Date()..., description: '', done: false */}
-          <h4>Agergar Todo</h4>
+          <h4>Agergar TODO</h4>
           <hr />
-          <form>
-            <input
-              type="text"
-              placeholder="¿Qué hay que hacer?"
-              className="form-control"
-            />
-            <button type="submit" className="btn btn-outline-primary mt-1">
-              Agregar
-            </button>
-          </form>
-          {/* fin TodoAdd  */}
+          <TodoAdd onNewTodo={handleNewTodo} />
         </div>
       </div>
     </>
