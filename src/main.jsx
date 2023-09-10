3859b0f700df1d48059eb27b4395006f92757134
Javier Moneo <jvr.moneo@gmail.com>
2023-09-10 11:36:48 +0200

    Preparando aplicacion con rutas - esqueleto

--- src/main.jsx
+++ src/main.jsx
@@ -14,12 +14,13 @@ import "./index.css";
 // import { SimpleForm } from "./02-useEffect/SimpleForm";
 // import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
 // import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
-import { TodoApp } from "./08-useReducer/TodoApp";
+// import { TodoApp } from "./08-useReducer/TodoApp";
+import { MainApp } from "./09-useContext/MainApp";
 
 // import "./08-useReducer/intro-reducer";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <TodoApp />
+  <MainApp />
   // </React.StrictMode>
 );
