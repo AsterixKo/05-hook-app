78c2aa280bfe1ccf5ccafb7161138fa9440b7441
Javier Moneo <jvr.moneo@gmail.com>
2023-09-27 12:55:45 +0200

    Pruebas de funciones del context

--- src/09-useContext/LoginPage.jsx
+++ src/09-useContext/LoginPage.jsx
@@ -3,20 +3,20 @@ import { UserContext } from "./context/UserContext";
 
 export const LoginPage = () => {
   const { user, setUser } = useContext(UserContext);
-  console.log(user);
+  // console.log(user);
   return (
     <>
       <h1>LoginPage</h1>
       <hr />
-      <pre>{JSON.stringify(user, null, 3)}</pre>
+      <pre aria-label="pre">{JSON.stringify(user, null, 3)}</pre>
 
       <button
         className="btn btn-primary"
         onClick={() =>
           setUser({
             id: 123,
-            name: "Fernando Herrera",
-            email: "fernando@google.com",
+            name: "Juan",
+            email: "juan@google.com",
           })
         }
       >
