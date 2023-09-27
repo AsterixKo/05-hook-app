8189e53565dc8f9f2afe7a34bf8603d7b8c59943
Javier Moneo <jvr.moneo@gmail.com>
2023-09-27 11:22:30 +0200

    Pruebas en los eventos del TodoItem

--- tests/08-Reducer/TodoItem.test.jsx
+++ tests/08-Reducer/TodoItem.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { TodoItem } from "../../src/08-useReducer/TodoItem";
 
 describe("Pruebas en <TodoItem />", () => {
@@ -33,5 +33,50 @@ describe("Pruebas en <TodoItem />", () => {
     const spanElement = screen.getByLabelText("span");
     expect(spanElement.className).toContain("align-self-center ");
     expect(spanElement.className).not.toContain("text-decoration-line-through");
+  });
+
+  test("debe de mostrar el Todo completado", () => {
+    todo.done = true;
+    render(
+      <TodoItem
+        todo={todo}
+        onToggleTodo={onToggleTodoMock}
+        onDeleteTodo={onDeleteTodoMock}
+      />
+    );
+
+    const spanElement = screen.getByLabelText("span");
+    expect(spanElement.className).toContain("text-decoration-line-through");
+  });
+
+  test("span debe de llamar el ToogleTodo cuando se hace click", () => {
+    render(
+      <TodoItem
+        todo={todo}
+        onToggleTodo={onToggleTodoMock}
+        onDeleteTodo={onDeleteTodoMock}
+      />
+    );
+    const spanElement = screen.getByLabelText("span");
+    fireEvent.click(spanElement);
+    expect(onToggleTodoMock).toHaveBeenCalledWith(todo.id);
+  });
+
+  test("span debe de llamar el deleteTodo", () => {
+    render(
+      <TodoItem
+        todo={todo}
+        onToggleTodo={onToggleTodoMock}
+        onDeleteTodo={onDeleteTodoMock}
+      />
+    );
+    const liElement = screen.getByRole("listitem");
+    // console.log(liElement.outerHTML);
+    // console.log(liElement.innerHTML);
+
+    const btnElement = screen.getByRole("button");
+    // console.log(btnElement.outerHTML);
+    fireEvent.click(btnElement);
+    expect(onDeleteTodoMock).toHaveBeenCalledWith(todo.id);
   });
 });
